8c02dc94b056de4576ba280a90245c71361c02f3
flower-snek <boomerangbro12@gmail.com>
2023-08-05 18:17:20 -0400

    color stuff, refactored note reading

--- pulsus/pulsus_interpreter.js
+++ pulsus/pulsus_interpreter.js
@@ -2,6 +2,14 @@
 const note_size = 72
 const maxline = Math.floor((innerWidth*0.8)/note_size)
 
+let colors = [
+	"#00afff", // base
+	"#ff2277", // chord
+	"#ff2222", // bad
+	"#ffcc22", // ?
+	"#22ff66" // good
+];
+
 let parent = document.getElementById("the_info");
 let note_objects = document.getElementsByClassName("p_notes");
 
@@ -34,7 +42,7 @@ let sketch_generator = function(nts){ // i have like 8 different things called n
 			let cs = note_size * 7/8
 			
 			for(let i = 0; i < nts.length; i++){
-				n = nts[i].n;
+				let n = nts[i][0].n;
 				if(n != "-"){
 					// draw bg square
 					s.fill(20)
@@ -42,17 +50,20 @@ let sketch_generator = function(nts){ // i have like 8 different things called n
 					let by = Math.floor(i / maxline)
 					let cx = note_size/2 + note_size*bx
 					let cy = note_size/2 + note_size*by
-					// console.log(nts[i])
+					//console.log(nts[i])
 					s.rect(cx, cy, cs, cs)
-					s.fill(nts[i].col)
-					for(let j = 0; j < n.length; j++){
-						let c = n.charAt(j)
+					for(let j = 0; j < nts[i].length; j++){
+						let note = nts[i][j]
+						// console.log(note)
+						let val = note.n
+						let col = note.col
 						//if(c == "!"){
 						//	s.fill("#dd4455")
 						//}else{
-							let num = parseInt(c) - 1
+							let num = parseInt(val) - 1
 							let x = (num % 3) - 1
 							let y = -(Math.floor(num / 3)) + 1
+							s.fill(col)
 							s.rect(cx + x*cs/3, cy + y*cs/3, cs*1/4, cs*1/4)
 						//}
 					}
@@ -65,46 +76,65 @@ let sketch_generator = function(nts){ // i have like 8 different things called n
 let interpret_pattern = function(txt){
 	txt = letter2number(txt)
 	let pattern = [];
-	let this_time = {
+	let this_time = [];
+	let this_note = {
 		n: "",
 		col: ""
-	}
+	};
+	let overall_color = ""
 	let pause = false;
 	for(let i = 0; i < txt.length; i++){
 		let skip = false;
 		let c = txt.charAt(i);
 		if(c == "["){
+			if(this_note.col != ""){
+				overall_color = this_note.col;
+			}
 			pause = true
+			skip = true
 		} else if(c == "]"){
 			pause = false
 		} else if(c == "!"){
-			this_time.col = "#ff0000"
+			this_note.col = colors[2]
 			skip = true;
 		} else if(c == "?"){
-			this_time.col = "#ffff00"
+			this_note.col = colors[3]
 			skip = true;
 		} else if(c == "^"){
-			this_time.col = "#00ff00"
+			this_note.col = colors[4]
 			skip = true;
 		} else {
-			this_time.n += c;
+			this_note.n = c;
 		}
 		
-		if(!pause && !skip){
-			// set color if not already set
-			if(this_time.col == ""){
-				this_time.col = "#ff0077";
-				if(this_time.n.length == 1){
-					this_time.col = "#00afff";
-				}
-			}
-			// console.log(this_time)
-			pattern.push(this_time);
-			
-			this_time = {
+		if(!skip){
+			if(this_note.n != ""){
+				this_time.push(this_note)
+			} // this is scuffed
+			this_note = {
 				n: "",
 				col: ""
 			}
+			
+			if(!pause){
+				// set color if not already set
+				for(let j = 0; j < this_time.length; j++){
+					if(overall_color != ""){
+						this_time[j].col = overall_color
+					}
+					if(this_time[j].col == ""){
+						this_time[j].col = colors[1]
+						if(this_time.length == 1){
+							this_time[j].col = colors[0];
+						}
+					}
+				};
+				//console.log(this_time)
+				pattern.push(this_time);
+				this_time = []
+				overall_color = "";
+			}
+			
 		}
 	}
 	if(pause){ // you forgot to put either a ] or something after a color indicator
@@ -114,6 +144,13 @@ let interpret_pattern = function(txt){
 	// console.log(pattern);
 	return pattern;
 }
+let update_colors = function(){
+	let color_inputs = document.getElementsByClassName("p_col");
+	for(let i = 0; i < color_inputs.length; i++){
+		colors[i] = "#" + color_inputs[i].value
+	}
+}
+
 /*
 let generator = document.getElementById("generator");
 if(generator){
@@ -143,6 +180,7 @@ note_objects.forEach((nobj) => {
 });
 
 let create_pattern = function(){
+	update_colors()
 	pattern = interpret_pattern(document.getElementById("p_in").value);
 	let canvas = sketch_generator(pattern);
 	let below = document.getElementById("below")
